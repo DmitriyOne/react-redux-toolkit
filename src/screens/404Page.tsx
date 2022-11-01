f4e72d58ed08145a305cb559b982f3b99ae4e61e
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-11-01 17:06:03 +0200

    Added animate open pages

--- src/screens/404Page.tsx
+++ src/screens/404Page.tsx
@@ -1,13 +1,13 @@
 import { FunctionComponent } from 'react'
 
-import { Heading } from '../components'
+import { AnimatedPage, Heading } from '../components'
 
 export const NotFoundPage: FunctionComponent = () => {
   return (
-    <>
+    <AnimatedPage>
       <Heading align='center'>
         404 Page
       </Heading>
-    </>
+    </AnimatedPage>
   )
 }
